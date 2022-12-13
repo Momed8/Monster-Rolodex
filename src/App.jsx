735162c6ca92e58ded2ebad9cd27e5c80425db1e
mohamed jniouhat <m.jniouhat@gmail.com>
2022-12-14 00:30:21 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter(monster =>
+      monster.name.toString().toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monster..."
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
